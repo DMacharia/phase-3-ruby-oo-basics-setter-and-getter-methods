85f1e082a3aa46f762986a79b3b404697f386c02
Daniel Macharia <danielhuios@gmail.com>
2022-10-21 19:22:50 +0300

    all test passed

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,26 @@
 class Person
+    # Setter
+    def name=(name)
+        @name = name
+    end
+    # Getter
+    def name
+        @name
+    end
 
+    
+    def job=(job)
+        @job = job
+    end
+    
+    def job
+        @job
+    end
 end
+
+daniel = Person.new
+daniel.name = "Daniel"
+daniel.job = "Programming"
+
+puts daniel.name
+puts daniel.job
